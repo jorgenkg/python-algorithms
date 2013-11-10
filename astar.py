f0a8b61ea0759ab245af3500c4b14fb3c7a2a1b9
Jørgen Grimnes <jorgenkg@gmail.com>
2013-11-10 10:17:38 +0100

    Faster Astart implementation

--- astar.py
+++ astar.py
@@ -1,58 +1,73 @@
-from Queue import PriorityQueue
+import heapq
+import math
+import itertools
 
-exists = {} # Keep track of existing nodes, so that one node may have multiple parents. Required to find optimal path.
 
-class AstarNode:
-   parent = None
-   f_score = None
-   h_score = None
-   is_expanded = False
-   is_observed = False
-   g_score = ( )
-   children = [ ]
-   
-   def generate_children(self):
-      # INITIATE children
-      n = Node()
-      if n.key not in exists.keys():
-         exists.update({n.key:n})
-      self.children.append(exists[n.key])
+exists = {} # Keep track of existing nodes, so that one node may have multiple parents. 
+            # This is required if you wish to find the optimal path.
+exist_key = set() # for a faster existence check.
+
+class AstarNode( object ):
+   def __init__(self, ):
+      self.parent = None
+      self.f_score = None
+      self.h_score = None
+      self.is_expanded = False
+      self.is_observed = False
+      self.g_score = ( )
+#end necessary values
+
 
 class Node( AstarNode ):
-    key = 'key_string' # requires a unique key for each node.
-    """
-        Here goes your node info
-    """
+   def __init__(self, ):
+      super(Node, self).__init__()
+      self.key = " " # unique key for hashing purposes.
 
-def heuristic(node,goal):
+   def generate_children(self):
+      """
+      Implement: generate children
+      """
+      children = []
+      for child in children:
+          node = Node( child )
+          if node.key not in exist_key:
+             exists.update({node.key:node})
+             exist_key.add(node.key)
+          children.append(exists[node.key])
+      return children
+#end
+
+
+def heuristic(node):
    """
-       Define a heuristic for you search
+   Implement: heuristc
    """
+   return 1
 
 def distance(parent,child):
    """
-       How far is it between these two neighbouring nodes?
-       This is the edge weight in a weighted graph
+   We dont have any definition of the distance between two nodes.
    """
+   return 1
+   
 
-def astar(start,goal):
-   queued_nodes = PriorityQueue()
+def astar(start):
    start.g_score = 0
-   start.h_score = heuristic(start,goal)
+   start.h_score = heuristic(start)
    start.f_score = start.g_score + start.h_score
    start.is_observed = True
-   queued_nodes.put((start.f_score,start))
+   queued_nodes = [ (start.f_score,start) ]
+   
    
    while not queued_nodes.empty():
-      current = queued_nodes.get()[1] # [f_score, node]
-      
+      current = heapq.heappop(queued_nodes)[1] # [f_score, node]
       if current.h_score == 0:
          return construct_path(current)
       else:
          current.is_observed = True
-         current.generate_children()
+         children = current.generate_children()
          current.is_expanded = True
-         for child in current.children:
+         for child in children:
             evaled_g_score = current.g_score + distance(current,child)
             if evaled_g_score<child.g_score:
                if child.is_expanded:
@@ -61,7 +76,7 @@ def astar(start,goal):
                   child.f_score = child.g_score + child.h_score
                   current.is_expanded = False
                   child.is_observed = False
-                  queued_nodes.put((child.f_score,child))
+                  heapq.heappush( queued_nodes, (child.f_score,child) )
                elif child.is_observed:
                   child.parent = current
                   child.g_score = evaled_g_score
@@ -69,14 +84,20 @@ def astar(start,goal):
                   current.is_observed = True
                   child.parent = current
                   child.g_score = evaled_g_score
-                  child.h_score = heuristic(child,goal)
+                  child.h_score = heuristic(child)
                   child.f_score = child.g_score + child.h_score
-                  queued_nodes.put((child.f_score,child))
+                  heapq.heappush( queued_nodes, (child.f_score,child) )
    return Exception('No result')
 
-
 def construct_path(node):
-   if node.parent:
-      return construct_path(node.parent) + [node]
-   else:
-      return [node]
+   tmp = []
+   while node.parent:
+       tmp.append( node.parent )
+       node = node.parent
+   return tmp
+
+
+"""
+Example usage:
+print astar(  )
+"""
